63b91a04885d14d1e3c0ad1faf71ddc7145c3ee0
LaiTkiLLa <84670285+LaiTkiLLa@users.noreply.github.com>
2022-07-07 12:01:10 +0300

    Update views.py

--- 2-2/books/views.py
+++ 2-2/books/views.py
@@ -1,8 +1,7 @@
 from django.core.paginator import Paginator
+from django.http import HttpResponse
 from django.shortcuts import render
-
 from books.models import Book
-
 
 
 def books_view(request):
@@ -13,10 +12,9 @@ def books_view(request):
     }
     return render(request, template, context)
 
-def pagi(request):
-    page_number = request.GET.get("page")
-    paginator = Paginator()
-    context = {
-        'pagin': pagi
-    }
-    return render(request, 'pagi,html')
+def one_book(request, some_book):
+    some_book == Book.pub_date
+    template = 'books/one_book.html'
+    books = Book.objects.all()
+    context = {"books" : books}
+    return render(request,template,context)
